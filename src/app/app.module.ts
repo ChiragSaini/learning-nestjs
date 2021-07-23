ff64c9a510b0a3a4a7ef79c6b266b801677a425b
Chirag Saini <chiragsaini7777@gmail.com>
2021-07-23 20:55:48 +0530

    Added Middleware and Swagger Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { StudentModule } from '../student/student.module';
 import { TeacherModule } from 'src/teacher/teacher.module';
-
 @Module({
   imports: [StudentModule, TeacherModule]
 })
